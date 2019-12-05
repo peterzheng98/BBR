e50ff452738ae042b1ee1aa120eb9d3f9c7af0e4
Peter Zheng <peterzheng98@sjtu.edu.cn>
2019-12-05 13:39:04 +0800

    some bugs in udp module

--- core/src/udpbinder.rs
+++ core/src/udpbinder.rs
@@ -1,23 +1,51 @@
 use std::env;
 use std::process;
-fn bindAddress(addr){
-    println!("Received {}", addr);
+use std::net::UdpSocket;
+fn listen(socket: &UdpSocket, mut buffer: &mut [u8]) -> usize{
+    let (number_of_bytes, src_addr) = socket.recv_from(&mut buffer).expect("Receives nothing.");
+    println!("{:?}", number_of_bytes);
+    println!("{:?}", src_addr);
+
+    number_of_bytes
+}
+
+fn send(socket: &UdpSocket, receiver: &str, msg: &Vec<u8>) -> usize{
+    println!("Sending data......");
+    let result = socket.send_to(msg, receiver).expect("Sending messages fail!");
+
+    result
+}
+
+fn bindAddress(addr : String) -> UdpSocket{
+    let mut socket = UdpSocket::bind(addr).expect("Cannot bind address.");
+    socket
 }
 
 fn main(){
     let args: Vec<String> = env::args().collect();
+    
     // Dispatch the arguments
     let address = &args[1];
     let mut mode: i8 = 0;
+    let mut buf: Vec<u8> = Vec::with_capacity(65536);
+    let message = String::from("abcdefghijlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+    let message_bytes = message.into_bytes();
     if &args[2] == "server" {
         mode = 0; // indicate that this is server
         println!("Server mode! {}", args[1]);
     } else if &args[2] == "client" {
         mode = 1; // indicate that this is client
-        println!("Client mode! {}", args[1]);
+        println!("\tClient mode! {}", args[1]);
+        let socket = bindAddress(String::from(address));
+        loop{
+            while listen(&socket, &mut buf) != 0{
+                println!("Waiting....");
+            }
+            send(&socket, address, &message_bytes);
+        }
     } else {
         println!("Usage: ./udpbinder <ip:port> <mode>");
         process::exit(0);
     }
-    bindAddress(address);
+    
 }
