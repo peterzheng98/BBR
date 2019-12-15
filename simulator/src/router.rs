ccacbbb903e27031186553fd0866537818dca930
Peter Zheng <peterzheng98@sjtu.edu.cn>
2019-12-15 11:32:44 +0800

    router p2p finish

--- simulator/src/router.rs
+++ simulator/src/router.rs
@@ -6,6 +6,26 @@ use std::thread;
 use std::collections;
 use std::time::Duration;
 
+fn unpackHeader(header : &[u8]) -> (i32, i32, i32){
+    let mut protocol = [0; 4];
+    let mut port1 = [0; 4];
+    let mut port2 = [0; 4];
+    let mut idx : usize = 0;
+    for iidx in 0..3{
+        protocol[iidx] = header[idx];
+        idx = idx + 1;
+    }
+    for iidx in 0..3{
+        port1[iidx] = header[idx];
+        idx = idx + 1;
+    }
+    for iidx in 0..3{
+        port2[iidx] = header[idx];
+        idx = idx + 1;
+    }
+    (i32::from_le_bytes(protocol), i32::from_le_bytes(port1), i32::from_le_bytes(port2))
+}
+
 // Assume this is a four-port router
 // 22001 - p2p exchange
 // 22002 - p2p exchange
@@ -26,7 +46,7 @@ fn main(){
     let p2pPort2 : i32 = i32::from_str(&arguments[2]).expect("Usage ./Router <P2P Port> <P2P Port> <Client Port> <Server Port> <Time>");
     let clientPort : i32 = i32::from_str(&arguments[3]).expect("Usage ./Router <P2P Port> <P2P Port> <Client Port> <Server Port> <Time>");
     let serverPort : i32 = i32::from_str(&arguments[4]).expect("Usage ./Router <P2P Port> <P2P Port> <Client Port> <Server Port> <Time>");
-    let delayTime : i32 = i32::from_str(&arguments[5]).expect()
+    let delayTime : i32 = i32::from_str(&arguments[5]).expect("Usage ./Router <P2P Port> <P2P Port> <Client Port> <Server Port> <Time>");
     let p2pPort1Addr = format!("127.0.0.1:{}", p2pPort1);
     let p2pPort2Addr = format!("127.0.0.1:{}", p2pPort2);
     let clientAddr = format!("127.0.0.1:{}", clientPort);
@@ -34,30 +54,48 @@ fn main(){
     const assumePackageSize : usize = 1024 - (1056 - 1024);
     println!("Configuration:\np2pAddr:{} <-> {}, Port:{} <-> {}\nClientAddr:{}, Port:{}\nServerAddr:{}, Port: {}", p2pPort1Addr, p2pPort2Addr, p2pPort1, p2pPort2, clientAddr, clientPort, serverAddr, serverPort);
     //Define socket here
-    let routerP2PSocket1 = UdpSocket::bind(format!("127.0.0.1:{}", p2pPort1));
-    let routerP2PSocket2 = UdpSocket::bind(format!("127.0.0.1:{}", p2pPort2));
-    let clientSocket = UdpSocket::bind(format!("127.0.0.1:{}", clientPort));
-    let serverSocket = UdpSocket::bind(format!("127.0.0.1:{}", serverPort));
-    routerP2PSocket1.set_nonblocking(true).unwrap();
-    routerP2PSocket2.set_nonblocking(true).unwrap();
-    clientSocket.set_nonblocking(true).unwrap();
-    serverSocket.set_nonblocking(true).unwrap();
+    let routerP2PSocket1 = UdpSocket::bind(format!("127.0.0.1:{}", p2pPort1)).unwrap();
+    let routerP2PSocket2 = UdpSocket::bind(format!("127.0.0.1:{}", p2pPort2)).unwrap();
+    let clientSocket = UdpSocket::bind(format!("127.0.0.1:{}", clientPort)).unwrap();
+    let serverSocket = UdpSocket::bind(format!("127.0.0.1:{}", serverPort)).unwrap();
     println!("Socket Bind Success!");
     println!("<-- Ready to serve -->");
     // stimulate the internal queue of the router
-    let mut internalQueue : collections::VecDeque<Vec(u8)> = collections::VecDeque::new();
+    let mut internalQueue : collections::VecDeque<Vec<u8>> = collections::VecDeque::new();
     let mut recvBuf1 = [0; assumePackageSize];
     let mut recvBuf2 = [0; assumePackageSize];
     let mut recvBuf3 = [0; assumePackageSize];
     let mut recvBuf4 = [0; assumePackageSize];
-    loop{
-        // Listen to client
-        let (amt1, src1) = routerP2PSocket1.recv_from(&mut recvBuf1).unwrap();
-        let (amt2, src2) = routerP2PSocket2.recv_from(&mut recvBuf2).unwrap();
-        let (amtC, srcC) = clientSocket.recv_from(&mut recvBuf3).unwrap();
-        let (amtS, srcS) = serverSocket.recv_from(&mut recvBuf4).unwrap();
-        
-    }
-
-
+    let udpProtocol = i32::from_le_bytes([1,0,0,0]);
+    let tcpProtocol = i32::from_le_bytes([1,1,0,0]);
+    // p2pPort only accept the packet with udp
+    let p2pPort1Thread = thread::spawn(move || {
+        let mut packageIdx = 0;
+        loop{
+            let (amt, src) = routerP2PSocket1.recv_from(&mut recvBuf1).unwrap();
+            let (protocol, targetPort, srcPort) = unpackHeader(&recvBuf1);
+            if protocol == udpProtocol {
+                routerP2PSocket1.send_to(&recvBuf1, format!("127.0.0.1:{}", targetPort));
+                println!("[{}# - {}] Get packet from {:?} to port {} with size {}. Forwarded.", 1, packageIdx, src, targetPort, amt);
+            } else {
+                println!("[{}# - {}] Get packet not fit the header, dropped.", 1, packageIdx)
+            }
+            packageIdx = packageIdx + 1;
+        }
+    });
+    let p2pPort2Thread = thread::spawn(move || {
+        let mut packageIdx = 0;
+        loop{
+            let (amt, src) = routerP2PSocket2.recv_from(&mut recvBuf2).unwrap();
+            let (protocol, targetPort, srcPort) = unpackHeader(&recvBuf2);
+            if protocol == udpProtocol {
+                routerP2PSocket2.send_to(&recvBuf2, format!("127.0.0.1:{}", targetPort));
+                println!("[{}# - {}] Get packet from {:?} to port {} with size {}. Forwarded.", 2, packageIdx, src, targetPort, amt);
+            } else {
+                println!("[{}# - {}] Get packet not fit the header, dropped.", 2, packageIdx)
+            }
+            packageIdx = packageIdx + 1;
+        }
+    });
+    let _ = p2pPort1Thread.join();
 }
