a23fba55a3e2ba2d8873c384b27146f259cf1066
Loya <13531665402@sjtu.edu.cn>
2019-12-16 14:38:00 +0800

    modify bbr

--- core/src/client.rs
+++ core/src/client.rs
@@ -82,8 +82,7 @@ fn main(){
         let mut seqNum : i32 = 0;
         let mut expectedAckNum : i32 = 0;
         while expectedAckNum < 512 * 1024{
-
-            // sent packet with 
+            // sent packet with
             let localPortInfo = clientPort.to_le_bytes();
             let serverPortInfo = serverPort.to_le_bytes();
             cwndCount = cwnd;
@@ -175,10 +174,11 @@ fn main(){
             println!("  -> Current seqNum:{}, expected ACK:{}", seqNum, expectedAckNum);
         }
         println!("Sent finished! Use time:{} seconds for 512MB", totalRunningTime.elapsed().unwrap().as_secs());
-    } else { // TCP BBR
+    }
+    else { // TCP BBR
         println!("  * Use BBR Algorithm");
         let mut ssthresh = 128;
-        let mut cwnd = 1;
+        let mut cwnd : i32 = 1;
         let mut cwndCount = cwnd;
         let clientSocket = UdpSocket::bind(format!("127.0.0.1:{}", clientPort)).unwrap();
         let totalRunningTime = SystemTime::now();
@@ -187,44 +187,50 @@ fn main(){
 
 
         //bw and inflight -> based on packet num, not size
-        let startup_pace = 2885.0 / 1000.0 + 1.0;
-        let drain_pace = 1000.0 / 2885.0;
+        let startup_pace : f64 = 2885.0 / 1000.0;
+        let drain_pace : f64= 1000.0 / 2885.0;
         let bw_pacing_gain = [5.0 / 4.0, 3.0 / 4.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0];
-        let rtt_pace = 1.0;
-        let mut state = 0; // 0: startup | 1: drain | probe_bw | probe_rtt
-        let mut cur_bw_pace_index = 0;
+        let rtt_pace:f64 = 1.0;
+        let mut state:i32 = 0; // 0: startup | 1: drain | probe_bw | probe_rtt
+        let mut cur_bw_pace_index : usize = 0;
 
-        let mut BtlBw_max = 0;
-        let mut RTprop_min = 1024.0;
+        let mut BtlBw_max : f64 = 0.0;
+        let mut RTprop_min : u128 = std::u128::MAX;
 
 //        let mut last_ack_time : collections::BTreeMap<i32, i32> = collections::BTreeMap::new();
-        let mut sendTime : collections::HashMap<i32, &SystemTime> = HashMap::new();
+        let mut sendTime : collections::HashMap<i32, SystemTime> = HashMap::new();
         let mut delivered_num : collections::HashMap<i32, i32> = HashMap::new();
-        let mut inflight_num = 0;
+        let mut inflight_num :i32 = 0;
+        let mut inflight : f64 = 0.0;
+        let mut sent_packet_num : i32 = 0;
 
-        let mut rtt_round = 0;
-        let mut rtt_round_seq_num = seqNum;
-        let mut last_round_bw = 0;
 
-        let mut sent_packet_num = 0;
+        let mut rtt_round : bool = false;
+        let mut rtt_round_seq_num: i32 = seqNum;
+        let mut last_round_bw: f64 = 0.0;
+        let mut cur_time : SystemTime = SystemTime::now();
+        let mut sent_time : SystemTime = SystemTime::now();
+
+        let mut rtt_modify_time : SystemTime = SystemTime::now();
+        let mut bw_low_increase_cnt : i32 = 0; //maximum = 3
+
+        let mut nextSendTime : SystemTime = SystemTime::now();
+        let mut sendMargin : u128 = 0;
+
         while expectedAckNum < 512 * 1024{
-
+            println!("-------------In a new Round----------");
             // sent packet with
             let localPortInfo = clientPort.to_le_bytes();
             let serverPortInfo = serverPort.to_le_bytes();
             cwndCount = cwnd;
-            let mut nextSendTime = SystemTime::now();
-            let mut sendMargin = 0;
+            println!("  Current cwnd is {}", cwnd);
+            let mut cur_bdp : f64 = BtlBw_max  * (RTprop_min as f64);
+            println!("  Current_State -> {}", state);
+            println!("  Current_BDP -> {}", cur_bdp);
             while cwndCount > 0{
-                let mut cur_bdp : f64 = BtlBw_max as f64 * RTprop_min;
-                println!("Current_BDP is {}", cur_bdp);
-                println!("Current_Inflight_Num is {}", inflight_num);
-                if inflight_num as f64 > cur_bdp {
-                    break;
-                }
-                println!("SystemTime Now is {:?}", SystemTime::now());
-                println!("NextSendTime is {:?}", nextSendTime);
-                if SystemTime::now() > nextSendTime {
+                inflight = inflight_num as f64 * 1024.0;
+                cur_time = SystemTime::now();
+                if cur_time > nextSendTime {
                     let mut sentBuf = [0; 1024];
                     let seqInfo = seqNum.to_le_bytes();
                     // TCP Header
@@ -252,18 +258,19 @@ fn main(){
                     }
                     // thread::sleep(Duration::new(0, time * 1000));
                     clientSocket.send_to(&sentBuf, format!("127.0.0.1:{}", routerPort));
-                    let mut cur_time = SystemTime::now();
-
+                    sent_time = SystemTime::now();
                     inflight_num = inflight_num + 1;
                     sent_packet_num = sent_packet_num + 1;
-                    sendTime.insert(seqNum, &cur_time);
+                    sendTime.insert(seqNum, sent_time);
                     delivered_num.insert(seqNum, sent_packet_num);
-                    nextSendTime = cur_time + Duration::new(sendMargin, 0);
-                    if rtt_round == 0{
+                    nextSendTime = sent_time + Duration::from_nanos(sendMargin as u64);
+
+                    if !rtt_round{
                         rtt_round_seq_num = seqNum;
-                        rtt_round = 1;
+                        rtt_round = true;
                     }
-                    println!("    - Client sent package with seq {}", seqNum);
+
+//                    println!("    - Client sent package with seq {}", seqNum);
                     seqNum = seqNum + 1;
                     cwndCount = cwndCount - 1;
                 }
@@ -273,66 +280,59 @@ fn main(){
             let mut duplicated : bool = false;
             let mut receivedACK : collections::BTreeMap<i32, i32> = collections::BTreeMap::new();
             let mut RecvBuf = [0; 1024];
-
-            let mut rtt_modify_time : SystemTime = SystemTime::now();
-            let mut rtt_modify_delta = 0;
-            let mut bw_low_increase_cnt = 0;
             while wait_ack_timeout.elapsed().unwrap().as_secs() < 2 && (!success_recv) {
                 // set for timeout
                 let (amt, src) = clientSocket.recv_from(&mut RecvBuf).unwrap();
                 let (protocol, port1, port2, ackCount, ackFlag) = unpackACK(&RecvBuf);
                 if protocol == tcpProtocol{
-                    let mut cur_time = SystemTime::now();
-                    let correspond_ackTime : SystemTime = **sendTime.get(&ackCount).unwrap();
-                    let correspond_ackDuration : Duration = correspond_ackTime.duration_since(correspond_ackTime).expect("Time failure in System::Time at flag 1");
-                    println!("correspond_ackDuration is {:?}", correspond_ackDuration);
-                    let mut cur_rtt : f64 = (correspond_ackDuration.as_nanos() as f64) /  1000000.0;
-//                    let mut cur_rtt = 0.1;
-                    // let mut cur_rtt = cur_time - Duration::new(sendTime.get(&ackCount), 0);
-                    let mut inflight_margin = sent_packet_num - *delivered_num.get(&ackCount).unwrap() + 1;
+
+                    let correspond_ackTime : SystemTime = *sendTime.get(&ackCount).unwrap();//todo
+                    let correspond_ackDuration : u128 = correspond_ackTime.elapsed().unwrap().as_nanos();
+
+                    let mut cur_rtt : u128 = correspond_ackDuration; //unit: ns
+                    let mut inflight_margin : i32 = sent_packet_num - *delivered_num.get(&ackCount).unwrap() + 1;
                     inflight_num = inflight_num - 1;
-                    println!("The delivered_num of ackCount {} is {}", ackCount, *delivered_num.get(&ackCount).unwrap());
-                    println!("inflight_margin is {}", inflight_margin);
-                    println!("cur_rtt is {}", cur_rtt);
-                    let mut cur_bw = inflight_margin as f64 / cur_rtt;
-                    if cur_bw > BtlBw_max as f64{
-                        BtlBw_max = cur_bw.round() as i64;
+
+                    let mut cur_bw : f64 = inflight_margin as f64 * 1024.0 / cur_rtt as f64; // bytes/ns
+
+                    if cur_bw > BtlBw_max{
+                        BtlBw_max = cur_bw;
                     }
-                    let currentTimeforRTT : SystemTime = SystemTime::now();
-                    rtt_modify_delta = (currentTimeforRTT.duration_since(rtt_modify_time).expect("Time failure in System::Time at flag 2")).as_secs();
-                    if rtt_modify_delta > 10{
-                        rtt_modify_delta = 0;
+                    let rtt_modify_delta: u128 = rtt_modify_time.elapsed().unwrap().as_nanos();
+                    if rtt_modify_delta > 10 * 1000000000{
                         rtt_modify_time = SystemTime::now();
                         RTprop_min = cur_rtt;
                         state = 3;
                     }
                     else if cur_rtt < RTprop_min && !(state == 3){
-                        rtt_modify_delta = 0;
                         rtt_modify_time = SystemTime::now();
                         RTprop_min = cur_rtt;
                     }
-                    let mut pace_rate = 0.0;
-                    let mut cur_bdp = BtlBw_max as f64 * RTprop_min;
+
+                    let mut pace_rate : f64 = 0.0;
+                    let mut cur_bdp = BtlBw_max * (RTprop_min as f64);
                     match state {
                         0 => {
-                            if ackCount == rtt_round_seq_num && rtt_round == 1{
-                                if cur_bw < (1.0 + 0.25) * last_round_bw as f64{
+                            if ackCount == rtt_round_seq_num && rtt_round{
+                                if cur_bw < (1.0 + 0.25) * last_round_bw{
                                     bw_low_increase_cnt = bw_low_increase_cnt + 1;
                                 }
-                                last_round_bw = cur_bw.round() as i64;
-                                rtt_round = 0;
+                                last_round_bw = cur_bw;
+                                rtt_round = false;
                             }
                             if bw_low_increase_cnt > 3{
                                 pace_rate = drain_pace;
+                                state = 1;
                                 bw_low_increase_cnt = 0;
                             }
                             else {
                                 pace_rate = startup_pace;
                             }
-                            cwnd = (cwnd as f64 * pace_rate) as i64;
+                            cwnd = (cwnd as f64 * pace_rate) as i32;
                         },
                         1 => {
-                            if cur_bdp >= inflight_num as f64{
+                            inflight = inflight_num as f64 * 1024.0;
+                            if cur_bdp >= inflight{
                                 state = 2;
                                 pace_rate = bw_pacing_gain[cur_bw_pace_index];
                                 cur_bw_pace_index = (cur_bw_pace_index + 1) % 8;
@@ -340,30 +340,34 @@ fn main(){
                             else{
                                 pace_rate = drain_pace;
                             }
-                            cwnd = (cwnd as f64 * pace_rate) as i64;
+                            cwnd = (cwnd as f64 * pace_rate) as i32;
                         },
 
                         2 => {
+                            inflight = inflight_num as f64 * 1024.0;
                             pace_rate = bw_pacing_gain[cur_bw_pace_index];
-                            let mut next_pace = 0;
+                            let mut next_pace : bool = false;
                             if pace_rate == 1.0{
                                 if cur_rtt > RTprop_min{
-                                    next_pace = 1;
+                                    next_pace = true;
                                 }
+
                             }
                             else if pace_rate > 1.0{
-                                if cur_rtt > RTprop_min && inflight_num as f64 >= cur_bdp{
-                                    next_pace = 1;
+                                if cur_rtt > RTprop_min && inflight >= cur_bdp{
+                                    next_pace = true;
                                 }
                             }
                             else{
-                                if cur_rtt > RTprop_min || inflight_num as f64 <= cur_bdp{
-                                    next_pace = 1;
+                                if cur_rtt > RTprop_min || inflight <= cur_bdp{
+                                    next_pace = true;
                                 }
+
                             }
-                            if next_pace == 1 {
+                            if next_pace {
                                 cur_bw_pace_index = (cur_bw_pace_index + 1) % 8;
                             }
+
                             cwnd = 2 * cwnd;
                         },
 
@@ -380,30 +384,12 @@ fn main(){
                         },
 
                     };
-                    sendMargin = (1024.0 / (pace_rate * (BtlBw_max as f64))).round() as u64;
-
+                    sendMargin = (1024.0 / (pace_rate * BtlBw_max)) as u128 ;
                     if ackCount == expectedAckNum{
                         expectedAckNum = expectedAckNum + 1;
-                        success_recv = true;
-                    } else {
-                        println!("in this mode");
-                        if !receivedACK.contains_key(&ackCount) {
-                            receivedACK.insert(ackCount, 1);
-                        } else {
-                            // the key exists
-                            if let Some(x) = receivedACK.get_mut(&ackCount) {
-                                *x += 1;
-                                if *x == 3{
-                                    duplicated = true;
-                                    success_recv = true;
-                                    seqNum = ackCount;
-                                }
-                            }
-                        }
-                        while receivedACK.contains_key(&expectedAckNum) {expectedAckNum = expectedAckNum + 1;}
                     }
-                    if expectedAckNum == seqNum - 1{
-                        println!("    - Client accept correct ACK, update cwnd");
+                    if expectedAckNum == seqNum{
+                        success_recv = true;
                     }
                 }
             }
